42360ae888d4b854270032b0e671e33b4c3b3f87
Neh Joshi <nehjoshi5@gmail.com>
2021-10-09 18:23:54 +0530

    Dark mode improved

--- js/script.js
+++ js/script.js
@@ -1,8 +1,22 @@
 const ck = document.getElementById("checkbox");
 const label = document.querySelector(".label");
 const ball = document.querySelector(".ball");
+const topWave = document.querySelector(".top-wave");
+const bottomWave = document.querySelector(".bottom-wave");
 ck.addEventListener("change", () => {
     document.body.classList.toggle("dark");
     label.classList.toggle("light");
     ball.classList.toggle("dark");
-})
+    if (bottomWave.getAttribute("fill") === "#404040"){
+        bottomWave.setAttribute("fill", "#0099ff");
+    }
+    else {
+        bottomWave.setAttribute("fill", "#404040");
+    }
+    if (topWave.getAttribute("fill") === "#404040"){
+        topWave.setAttribute("fill", "#0099ff");
+    }
+    else {
+        topWave.setAttribute("fill", "#404040");
+    }
+});
