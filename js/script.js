6badf579bf1051a788bebbf154336c527e702da9
Neh Joshi <nehjoshi5@gmail.com>
2021-10-22 14:19:45 +0530

    Responsive

--- js/script.js
+++ js/script.js
@@ -1,22 +1,25 @@
 const ck = document.getElementById("checkbox");
 const label = document.querySelector(".label");
 const ball = document.querySelector(".ball");
+const navWrapper = document.querySelector(".nav");
 const topWave = document.querySelector(".top-wave");
 const bottomWave = document.querySelector(".bottom-wave");
 ck.addEventListener("change", () => {
     document.body.classList.toggle("dark");
     label.classList.toggle("light");
     ball.classList.toggle("dark");
-    if (bottomWave.getAttribute("fill") === "#404040"){
-        bottomWave.setAttribute("fill", "#0099ff");
-    }
-    else {
-        bottomWave.setAttribute("fill", "#404040");
-    }
+    navWrapper.classList.toggle("dark");
+    // if (bottomWave.getAttribute("fill") === "#404040"){
+    //     bottomWave.setAttribute("fill", "#0099ff");
+    // }
+    // else {
+    //     bottomWave.setAttribute("fill", "#404040");
+    // }
     if (topWave.getAttribute("fill") === "#404040"){
         topWave.setAttribute("fill", "#0099ff");
     }
     else {
+        console.log("hello!");
         topWave.setAttribute("fill", "#404040");
     }
 });
