804e66d51540072c4b7e5752ae6a0c7169ca9d4f
Neh Joshi <nehjoshi5@gmail.com>
2021-10-23 16:58:19 +0530

    New Additions

--- js/script.js
+++ js/script.js
@@ -4,10 +4,12 @@ const ball = document.querySelector(".ball");
 const navWrapper = document.querySelector(".nav");
 const topWave = document.querySelector(".top-wave");
 const bottomWave = document.querySelector(".bottom-wave");
+const main = document.querySelector(".main");
 ck.addEventListener("change", () => {
     document.body.classList.toggle("dark");
     label.classList.toggle("light");
     ball.classList.toggle("dark");
+    main.classList.toggle("dark");
     navWrapper.classList.toggle("dark");
     // if (bottomWave.getAttribute("fill") === "#404040"){
     //     bottomWave.setAttribute("fill", "#0099ff");
